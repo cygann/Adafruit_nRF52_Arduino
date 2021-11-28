b3a5be5edb2fef2a116783859dd7ba5daa903225
Matthew Pauly <mjpauly@mac.com>
2021-11-28 11:20:03 -0800

    modify Feather Sense variant for Umbelt

--- variants/feather_nrf52840_sense/variant.cpp
+++ variants/feather_nrf52840_sense/variant.cpp
@@ -25,80 +25,87 @@
 
 const uint32_t g_ADigitalPinMap[] =
 {
-  // D0 .. D13
-  25,  // D0  is P0.25 (UART TX)
-  24,  // D1  is P0.24 (UART RX 
-  10,  // D2  is P0.10 (NFC2)
-  43,  // D3  is P1.11
-  42,  // D4  is P1.10 (LED2)
-  40,  // D5  is P1.08
-   7,  // D6  is P0.07
-  34,  // D7  is P1.02 (Button)
-  16,  // D8  is P0.16 (NeoPixel)
-  26,  // D9  is P0.26
-  27,  // D10 is P0.27
-   6,  // D11 is P0.06
-   8,  // D12 is P0.08
-  41,  // D13 is P1.09 (LED1)
+  // place in this list indicates which digital pin number is assigned and
+  // the value corresponds to the numbering used by the NRF52840:
+  // P0.00..P0.31 -> 0...31
+  // P1.00..P1.31 -> 32..63
 
-  // D14 .. D21 (aka A0 .. A7)
-   4,  // D14 is P0.04 (A0)
-   5,  // D15 is P0.05 (A1)
-  30,  // D16 is P0.30 (A2)
-  28,  // D17 is P0.28 (A3)
-   2,  // D18 is P0.02 (A4)
-   3,  // D19 is P0.03 (A5)
-  29,  // D20 is P0.29 (A6, Battery)
-  31,  // D21 is P0.31 (A7, ARef)
+  // D0 .. D1 (GPIO)
+  24,  // D0  is P0.24
+  25,  // D1  is P0.25
 
-  // D22 .. D23 (aka I2C pins)
-  12,  // D22 is P0.12 (SDA)
-  11,  // D23 is P0.11 (SCL)
+  // D2 .. D14 (aka MOTOR0 .. MOTOR12)
+  27,  // D2  is P0.27
+   0,  // D3  is P0.00
+   1,  // D4  is P0.01
+  26,  // D5  is P0.26
+   4,  // D6  is P0.04
+   5,  // D7  is P0.05
+   8,  // D8  is P0.08
+  40,  // D9  is P1.08
+  41,  // D10 is P1.09
+  14,  // D11 is P0.14
+  13,  // D12 is P0.13
+  16,  // D13 is P0.16
+  15,  // D14 is P0.15
 
-  // D24 .. D26 (aka SPI pins)
-  15,  // D24 is P0.15 (SPI MISO)
-  13,  // D25 is P0.13 (SPI MOSI)
-  14,  // D26 is P0.14 (SPI SCK )
+  // D15 .. D27 (aka MOTOR_EN0 .. MOTOR_EN12)
+  43,  // D15 is P1.11
+  45,  // D16 is P1.13
+  47,  // D17 is P1.15
+  31,  // D18 is P0.31
+  30,  // D19 is P0.30
+  28,  // D20 is P0.28
+  46,  // D21 is P1.14
+  36,  // D22 is P1.04
+  38,  // D23 is P1.06
+  39,  // D24 is P1.07
+  37,  // D25 is P1.05
+  35,  // D26 is P1.03
+  33,  // D27 is P1.01
+
+  // D28 (aka PIN_LED1)
+   9,  // D28 is P0.09
+
+  // D29 .. D30 (aka I2C pins)
+  12,  // D29 is P0.12 (SDA)
+  11,  // D30 is P0.11 (SCL)
+
+  // D31 .. D33 (aka A0 .. A2)
+  29,  // D31 is P0.29 (A0)
+   2,  // D32 is P0.02 (A1)
+   3,  // D33 is P0.03 (A2, Battery)
 
   // QSPI pins (not exposed via any header / test point)
-  19,  // D27 is P0.19 (QSPI CLK)
-  20,  // D28 is P0.20 (QSPI CS)
-  17,  // D29 is P0.17 (QSPI Data 0)
-  22,  // D30 is P0.22 (QSPI Data 1)
-  23,  // D31 is P0.23 (QSPI Data 2)
-  21,  // D32 is P0.21 (QSPI Data 3)
+  19,  // D34 is P0.19 (QSPI CLK)
+  20,  // D35 is P0.20 (QSPI CS)
+  21,  // D36 is P0.21 (QSPI Data 0)
+  32,  // D37 is P1.00 (QSPI Data 1)
+  22,  // D38 is P0.22 (QSPI Data 2)
+  23,  // D39 is P0.23 (QSPI Data 3)
 
-  // The remaining NFC pin
-   9,   // D33 is P0.09 (NFC1, exposed only via test point on bottom of board)
+  // Accelerometer IRQ
+  34,   // D40 is P0.09
 
-   0,   // D34 P0.00 is PDM Data
-   1,   // D35 P0.01 is PDM Clock
-  32,   // D36 P1.00 is APDS IRQ
+  // PDM Microphone
+   6,   // D41 P0.06 is PDM Data
+   7,   // D42 P0.07 is PDM Clock
 
   // The remaining pins are not usable:
   // The following pins were never listed as they were considered unusable
   // 18,      // P0.18 is RESET (attached to switch)
-  // 
+  //
   // The remaining pins are not connected (per schematic)
-  // 33,      // P1.01 is not connected per schematic
-  // 35,      // P1.03 is not connected per schematic
-  // 36,      // P1.04 is not connected per schematic
-  // 37,      // P1.05 is not connected per schematic
-  // 38,      // P1.06 is not connected per schematic
-  // 39,      // P1.07 is not connected per schematic
-  // 43,      // P1.11 is not connected per schematic
+  // 17,      // P0.17 is not connected per schematic
+  // 10,      // P0.10 is not connected per schematic
+  // 42,      // P1.10 is not connected per schematic
   // 44,      // P1.12 is not connected per schematic
-  // 45,      // P1.13 is not connected per schematic
-  // 46,      // P1.14 is not connected per schematic
 };
 
 void initVariant()
 {
-  // LED1 & LED2
+  // LED1
   pinMode(PIN_LED1, OUTPUT);
   ledOff(PIN_LED1);
-
-  pinMode(PIN_LED2, OUTPUT);
-  ledOff(PIN_LED2);
 }
 
--- variants/feather_nrf52840_sense/variant.h
+++ variants/feather_nrf52840_sense/variant.h
@@ -36,64 +36,69 @@ extern "C"
 #endif // __cplusplus
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (37)
-#define NUM_DIGITAL_PINS     (37)
-#define NUM_ANALOG_INPUTS    (6) // A6 is used for battery, A7 is analog reference
+#define PINS_COUNT           (43)
+#define NUM_DIGITAL_PINS     (43)
+#define NUM_ANALOG_INPUTS    (2) // A2 is used for battery
 #define NUM_ANALOG_OUTPUTS   (0)
 
+// Motor PWM control
+#define MOTOR0               (2)
+#define MOTOR1               (3)
+#define MOTOR2               (4)
+#define MOTOR3               (5)
+#define MOTOR4               (6)
+#define MOTOR5               (7)
+#define MOTOR6               (8)
+#define MOTOR7               (9)
+#define MOTOR8               (10)
+#define MOTOR9               (11)
+#define MOTOR10              (12)
+#define MOTOR11              (13)
+#define MOTOR12              (14)
+
+// Motor EN
+#define MOTOR_EN0            (15)
+#define MOTOR_EN1            (16)
+#define MOTOR_EN2            (17)
+#define MOTOR_EN3            (18)
+#define MOTOR_EN4            (19)
+#define MOTOR_EN5            (20)
+#define MOTOR_EN6            (21)
+#define MOTOR_EN7            (22)
+#define MOTOR_EN8            (23)
+#define MOTOR_EN9            (24)
+#define MOTOR_EN10           (25)
+#define MOTOR_EN11           (26)
+#define MOTOR_EN12           (27)
+
 // LEDs
-#define PIN_LED1             (13)
-#define PIN_LED2             (4)
-#define PIN_NEOPIXEL         (8)
-#define NEOPIXEL_NUM         1
+#define PIN_LED1             (28)
+#define NEOPIXEL_NUM         0
 
 #define LED_BUILTIN          PIN_LED1
-#define LED_CONN             PIN_LED2
-
-#define LED_RED              PIN_LED1
-#define LED_BLUE             PIN_LED2
+#define LED_CONN             PIN_LED1
 
 #define LED_STATE_ON         1         // State when LED is litted
 
-/*
- * Buttons
- */
-#define PIN_BUTTON1             (7)
-
 // Microphone
-#define PIN_PDM_DIN           34
-#define PIN_PDM_CLK           35
+#define PIN_PDM_DIN           41
+#define PIN_PDM_CLK           42
 #define PIN_PDM_PWR           -1  // not used
 
 /*
  * Analog pins
  */
-#define PIN_A0               (14)
-#define PIN_A1               (15)
-#define PIN_A2               (16)
-#define PIN_A3               (17)
-#define PIN_A4               (18)
-#define PIN_A5               (19)
-#define PIN_A6               (20)
-#define PIN_A7               (21)
+#define PIN_A0               (31)
+#define PIN_A1               (32)
+#define PIN_A2               (33)
 
 static const uint8_t A0  = PIN_A0 ;
 static const uint8_t A1  = PIN_A1 ;
 static const uint8_t A2  = PIN_A2 ;
-static const uint8_t A3  = PIN_A3 ;
-static const uint8_t A4  = PIN_A4 ;
-static const uint8_t A5  = PIN_A5 ;
-static const uint8_t A6  = PIN_A6 ;
-static const uint8_t A7  = PIN_A7 ;
 #define ADC_RESOLUTION    14
 
 // Other pins
-#define PIN_AREF           PIN_A7
-#define PIN_VBAT           PIN_A6
-#define PIN_NFC1           (33)
-#define PIN_NFC2           (2)
-
-static const uint8_t AREF = PIN_AREF;
+#define PIN_VBAT           PIN_A2
 
 /*
  * Serial interfaces
@@ -102,38 +107,23 @@ static const uint8_t AREF = PIN_AREF;
 #define PIN_SERIAL1_TX       (0)
 
 /*
- * SPI Interfaces
- */
-#define SPI_INTERFACES_COUNT 1
-
-#define PIN_SPI_MISO         (24)
-#define PIN_SPI_MOSI         (25)
-#define PIN_SPI_SCK          (26)
-
-static const uint8_t SS   = (5);
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
-
-/*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (22)
-#define PIN_WIRE_SCL         (23)
+#define PIN_WIRE_SDA         (29)
+#define PIN_WIRE_SCL         (30)
 
 // QSPI Pins
-#define PIN_QSPI_SCK         27
-#define PIN_QSPI_CS          28
-#define PIN_QSPI_IO0         29
-#define PIN_QSPI_IO1         30
-#define PIN_QSPI_IO2         31
-#define PIN_QSPI_IO3         32
+#define PIN_QSPI_SCK         34
+#define PIN_QSPI_CS          35
+#define PIN_QSPI_IO0         36
+#define PIN_QSPI_IO1         37
+#define PIN_QSPI_IO2         38
+#define PIN_QSPI_IO3         39
 
-// On-board QSPI Flash
-#define EXTERNAL_FLASH_DEVICES   GD25Q16C
-#define EXTERNAL_FLASH_USE_QSPI
+// Accel IRQ
+#define ACCEL_IRQ            40
 
 #ifdef __cplusplus
 }
